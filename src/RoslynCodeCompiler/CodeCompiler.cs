7d0d387fff100fda9fa16ca435da5b0cead5d1d2
Grey Wind <wjunqin7@gmail.com>
2025-04-06 03:16:24 +0800

    add interface

--- src/RoslynCodeCompiler/CodeCompiler.cs
+++ src/RoslynCodeCompiler/CodeCompiler.cs
@@ -5,7 +5,7 @@ namespace DotnetCodeCompiler
     /// <summary>
     /// Compile by calling the command line .NET project
     /// </summary>
-    public class CodeCompiler
+    public class CodeCompiler : ICompiler
     {
         #region private fields
         private readonly string net5ProjectContent = @"<Project Sdk=""Microsoft.NET.Sdk"">
@@ -121,7 +121,7 @@ namespace DotnetCodeCompiler
         /// </summary>
         /// <param name="outputPath">Output path</param>
         /// <exception cref="Exception">Compilation failure</exception>
-        public void CompileCode(string outputPath)
+        public virtual void CompileCode(string outputPath)
         {
             // 创建临时目录
             string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
@@ -241,9 +241,9 @@ namespace DotnetCodeCompiler
         /// Get locally available. NET version
         /// </summary>
         /// <returns>A list of all available.NET versions</returns>
-        public List<DotnetVersion> CheckLocalVersion()
+        public virtual List<DotnetVersion> CheckLocalVersion()
         {
-            List<string> versions = GetInstalledNet5PlusVersions();
+            List<string> versions = GetNetVersion.GetInstalledNet5PlusVersions();
             List<DotnetVersion> dotnetVersions = new();
 
             foreach (string version in versions)
@@ -293,110 +293,5 @@ namespace DotnetCodeCompiler
             return dotnetVersions;
         }
         #endregion
-
-        private static List<string> GetInstalledNet5PlusVersions()
-        {
-            var versions = new HashSet<string>();
-
-            try
-            {
-                // 执行命令获取运行时列表
-                string output = ExecuteDotnetListCommand();
-
-                // 逐行解析输出
-                foreach (string line in output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    if (TryParseNet5PlusVersion(line, out string version))
-                    {
-                        versions.Add(version);
-                    }
-                }
-            }
-            catch { /* 异常处理 */ }
-
-            // 转换为有序列表
-            var result = new List<string>(versions);
-            result.Sort(CompareVersions);
-            return result;
-        }
-
-        private static string ExecuteDotnetListCommand()
-        {
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = "dotnet",
-                Arguments = "--list-runtimes",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using var process = new Process { StartInfo = startInfo };
-            process.Start();
-            string output = process.StandardOutput.ReadToEnd();
-            process.WaitForExit(1000); // 3秒超时
-            return output;
-        }
-
-        private static bool TryParseNet5PlusVersion(string line, out string version)
-        {
-            version = null;
-
-            // 过滤有效运行时组件
-            if (!line.StartsWith("Microsoft.NETCore.App") &&
-                !line.StartsWith("Microsoft.AspNetCore.App") &&
-                !line.StartsWith("Microsoft.WindowsDesktop.App"))
-            {
-                return false;
-            }
-
-            // 提取版本号
-            string[] parts = line.Split(' ');
-            if (parts.Length < 2) return false;
-
-            try
-            {
-                var ver = new Version(parts[1]);
-                if (ver.Major < 5) return false; // 仅保留5+
-
-                version = $".NET {ver.Major}"; // 格式化为大版本号
-                return true;
-            }
-            catch { return false; }
-        }
-
-        private static int CompareVersions(string a, string b)
-        {
-            static int ExtractVersion(string s) => int.Parse(s.Replace(".NET ", ""));
-            return ExtractVersion(a).CompareTo(ExtractVersion(b));
-        }
-
-        private static bool TryParseMajorVersion(string line, out string version)
-        {
-            version = null;
-
-            // 匹配有效的运行时前缀
-            if (!line.StartsWith("Microsoft.NETCore.App") &&
-                !line.StartsWith("Microsoft.AspNetCore.App") &&
-                !line.StartsWith("Microsoft.WindowsDesktop.App"))
-            {
-                return false;
-            }
-
-            // 提取版本号
-            var segments = line.Split(' ');
-            if (segments.Length < 2) return false;
-
-            try
-            {
-                var ver = new Version(segments[1]);
-                version = $".NET {ver.Major}";  // 只取主版本号
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
     }
 }
---
+++ src/RoslynCodeCompiler/ICompiler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCodeCompiler
+{
+    /// <summary>
+    /// An interface for compiling code.
+    /// </summary>
+    public interface ICompiler
+    {
+        /// <summary>
+        /// Code to compile
+        /// </summary>
+        string Code { get; set; }
+
+        /// <summary>
+        /// Start compiling code
+        /// </summary>
+        /// <param name="outputPath">Output path</param>
+        /// <exception cref="Exception">Compilation failure</exception>
+        void CompileCode(string outputPath);
+        /// <summary>
+        /// Get locally available. NET version
+        /// </summary>
+        /// <returns>A list of all available.NET versions</returns>
+        List<CodeCompiler.DotnetVersion> CheckLocalVersion();
+    }
+}
